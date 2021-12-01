1472abe97c9c4f7c72a4a5876418d02564b78ce5
Katrina Liu <xiaol3@andrew.cmu.edu>
2021-12-01 00:08:32 -0500

    update armatus execute

--- src/armatus_execute.py
+++ src/armatus_execute.py
@@ -1,10 +1,19 @@
 import subprocess
+from os import listdir
 
-path = "../data/three-domains.txt.gz"
+data_path = "../data/"
 gamma = 0.5
-output = "results/test"
-args = ("./armatus","-i", path, "-g", str(gamma), "-o", output, "-m")
-popen = subprocess.Popen(args,stdout=subprocess.PIPE)
-popen.wait()
-output = popen.stdout.read()
-print(output)
+output_path = "results/"
+
+def execute(gamma):
+    for data_file in listdir(data_path):
+        path = "".join([data_path,data_file])
+        output = "".join([output_path,data_file])
+        args = ("./armatus","-i", path, "-g", str(gamma), "-o", output, "-m")
+        popen = subprocess.Popen(args,stdout=subprocess.PIPE)
+        popen.wait()
+        output = popen.stdout.read()
+        print(output)
+
+execute(gamma)
+
