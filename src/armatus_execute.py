ed45d4ec95b52a5dd454c701762f2c36291c6d69
Katrina Liu <xiaol3@andrew.cmu.edu>
2021-12-07 22:22:08 -0500

    Add recorded run time

--- src/armatus_execute.py
+++ src/armatus_execute.py
@@ -1,5 +1,6 @@
 import subprocess
 from os import listdir
+import time
 
 data_path = "../data/GSE66733_Hi-C_MCF7_MCF10A_processed_HiCfiles/40kb/"
 gamma = 0.5
@@ -10,10 +11,15 @@ def execute(gamma):
         path = "".join([data_path,data_file])
         output = "".join([output_path,data_file])
         args = ("./armatus","-i", path, "-g", str(gamma), "-o", output, "-m")
+        start = time.time()
         popen = subprocess.Popen(args,stdout=subprocess.PIPE)
         popen.wait()
+        f = open("time.txt", "a")
+        f.write("%s %f\n" % (data_file, time.time()-start))
+        print(time.time()-start)
         output = popen.stdout.read()
-        print(output)
+        #print(output)
+        print(output[str(output).find("user"):])
 
 execute(gamma)
 
